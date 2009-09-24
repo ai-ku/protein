a2643957a5b1aaf84722a3ea193822b29d1d8761
Deniz Yuret <denizyuret@gmail.com>
2009-09-24 09:48:15 +0000

    Use stdin as input. Require hermite.pl. Mode index starts at zero.

--- hermite/one.pl
+++ hermite/one.pl
@@ -2,70 +2,43 @@
 # Compute hermite expectations for single modes
 
 use strict;
-use Data::Dumper;
-$Data::Dumper::Indent = 0;
-
-my $Nm = 132;			# number of modes
+require "hermite.pl";
 my $Nh = 17;			# max degree of hermite
-my @E;				# expectations
-my @M;				# mode data
+my $Nt;				# number of time steps
+my $Nm;				# number of modes (dimensions)
 
-for (my $m = 1; $m <= $Nm; $m++) { # m = index of mode
-    open(FP, "Raw/hermite$m.dat") or die $!;
-    my @data = <FP>;
-    $M[$m] = \@data;
-    close(FP);
+warn "Reading mode data from stdin...\n";
+my @Xtm;			# mode data
+while(<>) {
+    my @x = split;
+    $Nm = scalar(@x) if not defined $Nm;
+    die unless scalar(@x) == $Nm;
+    push @Xtm, \@x;
+    $Nt++;
 }
 
+warn "Read $Nt time steps with $Nm modes.\n";
+
+warn "Computing xm^n expectations...\n";
+my @Xmn;			# expectations of xm^n
+for (my $m = 0; $m < $Nm; $m++) {
+    for (my $n = 0; $n <= $Nh; $n++) {
+	my $sum = 0;
+	for (my $t = 0; $t < $Nt; $t++) {
+	    $sum += expt($Xtm[$t][$m], $n);
+	}
+	$Xmn[$m][$n] = $sum / $Nt;
+    }
+}
+
+warn "Computing hermite polynomials...\n";
 for (my $h = 0; $h <= $Nh; $h++) { # h = degree of hermite
     my $H = hermite($h);
-    for (my $m = 1; $m <= $Nm; $m++) { # m = index of mode
+    for (my $m = 0; $m < $Nm; $m++) { # m = index of mode
 	my $sum = 0;
-	for (my $i = 0; $i <= $#{$H}; $i++) { # i = index of polynomial
-	    $sum += $H->[$i] * expectation($m, $i);
+	for (my $n = 0; $n <= $#{$H}; $n++) { # n = index of polynomial
+	    $sum += $H->[$n] * $Xmn[$m][$n];
 	}
 	print "$h\t$m\t$sum\n";
     }
-}
-
-sub expectation {
-    my ($m, $i) = @_;
-    if (not defined $E[$m][$i]) {
-	my $sum = 0;
-	$sum += expt($_, $i) for @{$M[$m]};
-	$E[$m][$i] = $sum / scalar(@{$M[$m]});
-    }
-    return $E[$m][$i];
-}
-
-sub hermite {
-    # H0(x) = 1, H1(x) = x, H[p+1](x) = x Hp(x) - p H[p-1](x)
-
-    my ($h) = @_;		# degree of hermite
-    my $H = [];			# polynomial coefficient array
-
-    if ($h < 0) {
-	die;
-    } elsif ($h == 0) {
-	$H = [1];
-    } elsif ($h == 1) {
-	$H = [0, 1];
-    } else {
-	my $H1 = hermite($h - 1);
-	my $H2 = hermite($h - 2);
-	unshift @$H1, 0;	# multiply by x
-	$_ *= (1 - $h) for @$H2;
-	for (my $i = 0; $i <= $#{$H1}; $i++) {
-	    $H->[$i] = $H1->[$i] + ($H2->[$i] or 0);
-	}
-    }
-    return $H;
-}
-
-sub expt {
-    my ($x, $n) = @_;
-    my $xx = ($x >= 0 ? $x : -$x);
-    my $ans = exp($n * log($xx));
-    $ans = -$ans if (($x < 0) and ($n % 2));
-    return $ans;
 }
