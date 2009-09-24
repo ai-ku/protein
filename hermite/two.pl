9ab7a03a75355fee0713093e577499880af3b791
Deniz Yuret <denizyuret@gmail.com>
2009-09-24 12:28:37 +0000

    *** empty log message ***

--- hermite/two.pl
+++ hermite/two.pl
@@ -2,34 +2,54 @@
 # Compute hermite expectations for mode pairs
 
 use strict;
-use Data::Dumper;
-$Data::Dumper::Indent = 0;
+use PDL;
+use PDL::NiceSlice;
+require "hermite.pl";
 
-my $Nm = 132;			# number of modes
 my $Nh = 17;			# max degree of hermite
-my @M;				# mode data
-my %E;				# expectations
+my $Nt;				# number of time steps
+my $Nm;				# number of modes (dimensions)
+my $Xmt;			# data matrix
 
-for (my $m = 1; $m <= $Nm; $m++) { # m = index of mode
-    open(FP, "Raw/hermite$m.dat") or die $!;
-    my @data = <FP>;
-    $M[$m] = \@data;
-    close(FP);
+warn "Reading mode data from stdin...\n";
+my @data;
+while(<>) {
+    push @data, pdl(split);
+}
+$Xmt = pdl(@data);
+($Nm, $Nt) = dims($Xmt);
+warn "Read $Nt time steps with $Nm modes.\n";
+
+warn "Computing xm1^n1 * xm2^n2 expectations...\n";
+my %E2;
+for (my $m1 = 0; $m1 < $Nm; $m1++) { # m1 = index of first mode
+    my $xm1 = $Xmt($m1,:);
+    for (my $m2 = 0; $m2 < $m1; $m2++) { # m2 = index of second mode
+	my $xm2 = $Xmt($m2,:);
+	warn "$m1\t$m2\n";
+	for (my $n1 = 0; $n1 < $Nh; $n1++) { # n1 = power of first mode
+	    for (my $n2 = 0; $n2 <= $Nh-$n1; $n2++) { # n2 = power of second mode
+		$E2{$m1,$n1,$m2,$n2} = avg(($xm1 ** $n1) * ($xm2 ** $n2));
+	    }
+	}
+    }
 }
 
-for (my $h1 = 0; $h1 <= $Nh; $h1++) { # h1 = degree of first hermite
+warn "Computing hermite polynomials...\n";
+for (my $h1 = 1; $h1 < $Nh; $h1++) { # h1 = degree of first hermite
     my $H1 = hermite($h1);
-    for (my $h2 = 0; $h2 <= $Nh-$h1; $h2++) { # h2 = degree of second hermite
+    for (my $h2 = 1; $h2 <= $Nh-$h1; $h2++) { # h2 = degree of second hermite
+	next if ($h1 == 1 and $h2 == 1);
 	my $H2 = hermite($h2);
-	print STDERR "$h1\t$h2\n";
-	for (my $m1 = 1; $m1 <= $Nm; $m1++) { # m1 = index of first mode
-	    for (my $m2 = 1; $m2 <= $Nm; $m2++) { # m2 = index of second mode
+	warn "$h1\t$h2\n";
+	for (my $m1 = 0; $m1 < $Nm; $m1++) { # m1 = index of first mode
+	    for (my $m2 = 0; $m2 < $m1; $m2++) { # m2 = index of second mode
 		my $sum = 0;
-		for (my $i1 = 0; $i1 <= $#{$H1}; $i1++) { # index into first polynomial
-		    next if $H1->[$i1] == 0;
-		    for (my $i2 = 0; $i2 <= $#{$H2}; $i2++) { # index into second polynomial
-			next if $H2->[$i2] == 0;
-			$sum += $H1->[$i1] * $H2->[$i2] * expectation($m1, $i1, $m2, $i2);
+		for (my $n1 = 0; $n1 <= $#{$H1}; $n1++) { # index into first polynomial
+		    next if $H1->[$n1] == 0;
+		    for (my $n2 = 0; $n2 <= $#{$H2}; $n2++) { # index into second polynomial
+			next if $H2->[$n2] == 0;
+			$sum += $H1->[$n1] * $H2->[$n2] * $E2{$m1,$n1,$m2,$n2};
 		    }
 		}
 		print "$h1\t$h2\t$m1\t$m2\t$sum\n";
@@ -38,50 +58,3 @@ for (my $h1 = 0; $h1 <= $Nh; $h1++) { # h1 = degree of first hermite
     }
 }
 
-sub expectation {
-    my ($m1, $i1, $m2, $i2) = @_;
-    my $key = "$m1,$i1,$m2,$i2";
-    if (not defined $E{$key}) {
-	my $sum = 0;
-	my $len = @{$M[$m1]};
-	for (my $i = 0; $i < $len; $i++) {
-	    my $x1 = $M[$m1][$i];
-	    my $x2 = $M[$m2][$i];
-	    $sum += expt($x1, $i1) * expt($x2, $i2);
-	}
-	$E{$key} = $sum / $len;
-    }
-    return $E{$key};
-}
-
-sub hermite {
-    # H0(x) = 1, H1(x) = x, H[p+1](x) = x Hp(x) - p H[p-1](x)
-
-    my ($h) = @_;		# degree of hermite
-    my $H = [];			# polynomial coefficient array
-
-    if ($h < 0) {
-	die;
-    } elsif ($h == 0) {
-	$H = [1];
-    } elsif ($h == 1) {
-	$H = [0, 1];
-    } else {
-	my $H1 = hermite($h - 1);
-	my $H2 = hermite($h - 2);
-	unshift @$H1, 0;	# multiply by x
-	$_ *= (1 - $h) for @$H2;
-	for (my $i = 0; $i <= $#{$H1}; $i++) {
-	    $H->[$i] = $H1->[$i] + ($H2->[$i] or 0);
-	}
-    }
-    return $H;
-}
-
-sub expt {
-    my ($x, $n) = @_;
-    my $xx = ($x >= 0 ? $x : -$x);
-    my $ans = exp($n * log($xx));
-    $ans = -$ans if (($x < 0) and ($n % 2));
-    return $ans;
-}
