3d3984188beaf634c69b92057f736595876bf453
Deniz Yuret <denizyuret@gmail.com>
2010-04-17 21:06:18 +0000

    *** empty log message ***

--- hermite/f1analyze.pl
+++ hermite/f1analyze.pl
@@ -6,6 +6,6 @@ my $logf0 = -187.3/$nmode;
 while(<>) {
     my ($nd, $logp, $nneg) = /^(\d+) 996 instances, \d+ positive probabilities \(avg logP=(-\d\.\d+)\), (\d+) negative probabilities$/
 	or die;
-    my $tot = $nd * (-$logp) + ($nmode - $nd) * (-$logf0);
+    my $tot = $nd * $logp + ($nmode - $nd) * $logf0;
     print "$nd\t$tot\t$nneg\n";
 }
